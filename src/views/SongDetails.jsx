1e4a7231c57077f2f3cd0a34562632dc4d452c1e
Niv <bniv000@gmail.com>
2022-11-14 09:20:54 +0200

    fixed key prop and theme color

--- src/views/SongDetails.jsx
+++ src/views/SongDetails.jsx
@@ -25,13 +25,12 @@ export const SongDetails = () => {
 
     return <div className="flex flex-col">
         <DetailsHeader artistId="" songData={songData} />
-
         <div className="mb-10">
             <h2 className="text-white text-3xl font-bold">Lyrics</h2>
             <div className="mt-5">
                 {songData?.sections[1].type === 'LYRICS' ? songData?.sections[1].text.map((line, idx) => (
-                    <p className="text-gray-400 text-base my-1">{line}</p>
-                )) : <p className="text-gray-400 text-base my-1">Sorry no lyrics available for this song</p>}
+                    <p key={idx} className="text-gray-400 text-base my-1">{line}</p>
+                )) : <p key={idx} className="text-gray-400 text-base my-1">Sorry no lyrics available for this song</p>}
             </div>
         </div>
 
